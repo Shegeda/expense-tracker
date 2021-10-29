e8c49b53d259ce2b7a42b67247207ad5395a2d87
Shegeda <shegedasasha@gmail.com>
2021-10-29 19:49:02 +0300

    v1.0 start dev expense-tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseList from "./components/ExpenseList";
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Marathon starter kit',
+      amount: 29,
+      date: new Date(2020, 7, 14)
+    },
+    {
+      id: 'e2',
+      title: 'New AirPods 3',
+      amount: 179,
+      date: new Date(2021, 2, 12)
+    },
+    {
+      id: 'e3',
+      title: 'Getmancar',
+      amount: 77,
+      date: new Date(2021, 2, 28)
+    },
+    {
+      id: 'e4',
+      title: 'New MacBook Pro 14" 512Gb',
+      amount: 1999,
+      date: new Date(2021, 5, 12)
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseList items={expenses} />
     </div>
   );
 }
